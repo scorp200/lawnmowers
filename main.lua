5017e66ac03b45c1567e52ca02aad48b32f36b07
Anton K. (ai Doge) <aidoge@aidoge.xyz>
2022-04-30 00:12:06 -0400

    draw grass

--- main.lua
+++ main.lua
@@ -1,8 +1,13 @@
 local width = 15
 local height = 30
 local size = 64
-
+local spriteSheet
+local sprites = {}
 function love.load()
+  spriteSheet = love.graphics.newImage('assets/sprite.png', nil)
+
+  sprites['grass'] = love.graphics.newQuad(0, 0, 64, 64, spriteSheet:getDimensions())
+
   width = math.floor(love.graphics.getWidth() / size)
   height = math.floor(love.graphics.getHeight() / size)
 end
@@ -13,7 +18,7 @@ function love.draw()
   for x = 1, width do
     for y = 1, height do
       love.graphics.setColor(.2, .9, .3)
-      love.graphics.rectangle('fill', (x - 1) * size, (y - 1) * size, edge, edge)
+      love.graphics.draw(spriteSheet, sprites.grass, (x - 1) * size, (y - 1) * size)
     end
   end
 end
