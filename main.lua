287dad7065da6a7dbe0aac2ee37894c04657b2a1
Anton K. (ai Doge) <abca2004@gmail.com>
2022-04-29 22:29:36 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,21 @@
+local width = 15
+local height = 30
+local size = 64
+
+function love.load()
+  width = math.floor(love.graphics.getWidth() / size)
+  height = math.floor(love.graphics.getHeight() / size)
+end
+
 function love.draw()
-  love.graphics.print('Helxvex Computer Sucks')
+  local edge = size - 1
+
+  for x = 1, width do
+    for y = 1, height do
+      love.graphics.setColor(.2, .9, .3)
+      love.graphics.rectangle('fill', (x - 1) * size, (y - 1) * size, edge, edge)
+    end
+  end
 end
 
 function love.update(dt)
